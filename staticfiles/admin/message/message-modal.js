c14ac6f481d2e6662940f63d6d663cea4700267b
chorhksdlr <chorhksdlr@gmail.com>
2024-02-03 15:43:41 +0900

    쪽지관리

--- staticfiles/admin/message/message-modal.js
+++ staticfiles/admin/message/message-modal.js
@@ -1,11 +1,30 @@
 // 모달창
-const modalOpenButtons = document.querySelectorAll(
+const modalMessageOpenButtons = document.querySelectorAll(
     ".member-message-list-button"
 );
-const modalCloseButtons = document.querySelectorAll(
+const modalMessageCloseButtons = document.querySelectorAll(
     ".admin-message-modal-left-button"
 );
-const modal = document.getElementById("admin-message-modal");
+const messagemodal = document.getElementById("admin-message-modal");
+
+modalMessageOpenButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        messagemodal.classList.remove("hidden");
+    });
+});
+
+modalMessageCloseButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        messagemodal.classList.add("hidden");
+    });
+});
+
+// 모달창
+const modalOpenButtons = document.querySelectorAll(".member-user-list-button");
+const modalCloseButtons = document.querySelectorAll(
+    ".admin-user-modal-left-button"
+);
+const modal = document.getElementById("admin-user-modal");
 
 modalOpenButtons.forEach((button) => {
     button.addEventListener("click", () => {
