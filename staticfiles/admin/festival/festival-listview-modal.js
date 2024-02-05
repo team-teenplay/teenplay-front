0ec9acdb17ff73e05e47b4bddc6a413585853604
chorhksdlr <chorhksdlr@gmail.com>
2024-02-05 09:17:02 +0900

    축제관리 중간

---
+++ staticfiles/admin/festival/festival-listview-modal.js
@@ -0,0 +1,31 @@
+// 모달창
+const modalOpenButtons = document.querySelectorAll(".member-user-list-button");
+const modalCloseButtons = document.querySelectorAll(
+    ".admin-user-modal-left-button"
+);
+const modalAddCloseButtons = document.querySelectorAll(
+    ".admin-user-modal-right-button"
+);
+const modal = document.getElementById("admin-user-modal");
+const modalBack = document.getElementById("admin-user-modal-backdrop");
+
+modalOpenButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        modal.classList.remove("hidden");
+        modalBack.classList.remove("hidden");
+    });
+});
+
+modalCloseButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        modal.classList.add("hidden");
+        modalBack.classList.add("hidden");
+    });
+});
+
+modalAddCloseButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        modal.classList.add("hidden");
+        modalBack.classList.add("hidden");
+    });
+});
