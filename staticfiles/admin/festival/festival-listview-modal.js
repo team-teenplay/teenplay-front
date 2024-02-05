1916d037b8a0d4aa84e983e56fa79392af96fc15
chorhksdlr <chorhksdlr@gmail.com>
2024-02-05 14:28:18 +0900

    축제관리 상세완료

--- staticfiles/admin/festival/festival-listview-modal.js
+++ staticfiles/admin/festival/festival-listview-modal.js
@@ -10,7 +10,11 @@ const modal = document.getElementById("admin-user-modal");
 const modalBack = document.getElementById("admin-user-modal-backdrop");
 
 modalOpenButtons.forEach((button) => {
-    button.addEventListener("click", () => {
+    button.addEventListener("click", (event) => {
+        const targetId = event.currentTarget.getAttribute("data-target");
+        currentTargetLi = document.querySelector(`li[data-id="${targetId}"]`);
+
+        // 모달 열기
         modal.classList.remove("hidden");
         modalBack.classList.remove("hidden");
     });
@@ -22,10 +26,22 @@ modalCloseButtons.forEach((button) => {
         modalBack.classList.add("hidden");
     });
 });
+const confirmDeleteButtons = document.querySelectorAll(
+    ".admin-user-modal-right-button"
+);
 
-modalAddCloseButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-        modal.classList.add("hidden");
-        modalBack.classList.add("hidden");
+if (confirmDeleteButtons.length > 0) {
+    confirmDeleteButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            // 현재 대상 li 삭제
+            if (currentTargetLi) {
+                currentTargetLi.remove();
+                currentTargetLi = null;
+            }
+
+            // 모달 닫기
+            modal.classList.add("hidden");
+            modalBack.classList.add("hidden");
+        });
     });
-});
+}
