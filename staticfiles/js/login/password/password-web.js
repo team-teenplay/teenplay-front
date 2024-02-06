6b5bac9e4cf543d86547fbe52da18fc4ec689517
songseogyeong <0307ssk@naver.com>
2024-02-06 17:43:37 +0900

    main/password finish

--- staticfiles/js/login/password/password-web.js
+++ staticfiles/js/login/password/password-web.js
@@ -8,7 +8,15 @@ const inputItem = document.querySelector(".input-item");
 const inputError = document.querySelector(".input-error")
 
 // (예시) 버튼 클릭 시 오류 메세지 출력
+// emailSend.addEventListener("click", () => {
+//     inputError.classList.remove("hidden");
+//     inputItem.style = "border-color: #CE201B";
+// });
+
 emailSend.addEventListener("click", () => {
-    inputError.classList.remove("hidden");
-    inputItem.style = "border-color: #CE201B";
+    Swal.fire(
+        "비밀번호 재설정 메일 발송 완료",
+        "입력하신 메일로 수신하신 링크를 통해 <br> 비밀번호를 재설정하실 수 있습니다.",
+        "success"
+    );
 });
