65b6e4ae7261654413dcb98fe9b620babf0b548e
chorhksdlr <chorhksdlr@gmail.com>
2024-02-07 10:50:09 +0900

    전체모임관리

--- staticfiles/js/admin/meeting/admin-meeting-addimg.js
+++ staticfiles/js/admin/meeting/admin-meeting-addimg.js
@@ -15,3 +15,21 @@ imageInput.addEventListener("change", function (event) {
         reader.readAsDataURL(file);
     }
 });
+
+const backimageInput = document.getElementById("backimageInput");
+const backPhotoImg = document.querySelector("#main-post-photo-backimg");
+
+backimageInput.addEventListener("change", function (event) {
+    const file = event.target.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+            backPhotoImg.src = e.target.result;
+            backPhotoImg.style.display = "block";
+        };
+
+        reader.readAsDataURL(file);
+    }
+});
