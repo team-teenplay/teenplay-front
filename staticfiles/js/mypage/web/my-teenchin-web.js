648a9a5f262b5aca6f7239916ef42305cea51463
kjkj0724 <kjkj0724@gmail.com>
2024-02-17 15:35:54 +0900

    all-modify fix4

--- staticfiles/js/mypage/web/my-teenchin-web.js
+++ staticfiles/js/mypage/web/my-teenchin-web.js
@@ -22,9 +22,10 @@ tabs.forEach((tab) => {
 // 틴친중, 신청중 버튼 클릭 시 발생 하는 이벤트
 const teenchinModalWrap = document.querySelector(".teenchin-modal-wrap");
 const teenchinModalContainer = document.querySelector(".teenchin-modal-container");
-const teenchinBtns = document.querySelectorAll(".teenchin-btn");
 
 // 틴친중
+const teenchinBtns = document.querySelectorAll(".teenchin-btn");
+
 teenchinBtns.forEach((teenchinBtn) => {
     teenchinBtn.addEventListener("click", (e) => {
         let profileName = e.target.closest(".teenchin-items").querySelector(".profile-name").innerText;
@@ -48,35 +49,85 @@ teenchinWaitBtns.forEach((teenchinWaitBtn) => {
     });
 });
 
+// 수락하기
+const teenchinAgreeBtns = document.querySelectorAll(".teenchin-agree-btn");
+
+teenchinAgreeBtns.forEach((teenchinAgreeBtn) => {
+    teenchinAgreeBtn.addEventListener("click", (e) => {
+        let profileName = e.target.closest(".teenchin-items").querySelector(".profile-name").innerText;
+        madalContent(e, profileName);
+
+        teenchinModalContainer.style.animation = "popUp 0.5s";
+        teenchinModalWrap.style.display = "flex";
+    });
+});
+
 // 버튼의 클래스에 따라 모달창 내용을 변경해주는 함수
 const teenchinModalTitle = document.querySelector(".teenchin-modal-title");
-const teenchinModalBtn = document.querySelector(".teenchin-modal-btn");
+const teenchinModalCancleBtn = document.querySelector(".teenchin-modal-cancle-btn");
+const teenchinModalChangedBtn = document.querySelector(".teenchin-modal-changed-btn");
 const modalHeaderTitle = teenchinModalWrap.querySelector(".modal-header-title");
 
 const madalContent = (e, profileName) => {
+    if (e.target.closest(".teenchin-agree-btn")) {
+        teenchinModalTitle.innerText = `${profileName}님의\n틴친 요청을 수락 하시겠습니까?`;
+        teenchinModalCancleBtn.innerText = `거절하기`;
+        teenchinModalCancleBtn.classList.add("refuse-btn");
+        teenchinModalChangedBtn.innerText = `수락하기`;
+        teenchinModalChangedBtn.classList.add("agree-btn");
+        modalHeaderTitle.innerText = `${profileName}님의`;
+        return;
+    }
+
     if (e.target.closest(".teenchin-btn")) {
         teenchinModalTitle.innerText = `${profileName}님과\n틴친을 취소 하시겠습니까?`;
-        teenchinModalBtn.innerText = `틴친끊기`;
+        teenchinModalChangedBtn.innerText = `틴친끊기`;
         modalHeaderTitle.innerText = `더 이상 ${profileName}님과\n틴친관계가 아닙니다.`;
-    } else if (e.target.closest(".teenchin-wait-btn")) {
+        return;
+    }
+
+    if (e.target.closest(".teenchin-wait-btn")) {
         teenchinModalTitle.innerText = `${profileName}님에게\n보낸 틴친 신청을 취소하시겠습니까?`;
-        teenchinModalBtn.innerText = `취소하기`;
+        teenchinModalChangedBtn.innerText = `취소하기`;
         modalHeaderTitle.innerText = `${profileName}님에게\n보낸 틴친 신청을 취소했습니다.`;
     }
 };
 
+// 틴친끊기, 취소하기 클릭 시 기존 모달을 닫고 확인모달을 나타내는 이벤트
 const teenchinModalBtns = teenchinModalContainer.querySelectorAll("button");
 const teenchinCheckModalContainer = teenchinModalWrap.querySelector(".teenchin-check-modal-container");
 
 teenchinModalBtns.forEach((teenchinModalBtn) => {
     teenchinModalBtn.addEventListener("click", (e) => {
+        teenchinModalContainer.style.animation = "popDown 0.5s";
         if (e.target.closest(".teenchin-modal-cancle-btn")) {
-            teenchinModalContainer.style.animation = "popDown 0.5s";
+            if (e.target.closest(".teenchin-modal-cancle-btn").classList.contains("refuse-btn")) {
+                modalHeaderTitle.innerText += `\n틴친 요청을 거절하셨습니다.`;
+
+                setTimeout(() => {
+                    teenchinModalCancleBtn.innerText = `닫기`;
+                    teenchinModalCancleBtn.classList.remove("refuse-btn");
+                    teenchinModalChangedBtn.classList.remove("agree-btn");
+                    teenchinModalContainer.style.display = "none";
+                    teenchinCheckModalContainer.style.animation = "popUp 0.5s";
+                    teenchinCheckModalContainer.style.display = "flex";
+                }, 500);
+                return;
+            }
             setTimeout(() => {
                 teenchinModalWrap.style.display = "none";
             }, 500);
         } else {
-            teenchinModalContainer.style.animation = "popDown 0.5s";
+            if (e.target.closest(".teenchin-modal-changed-btn").classList.contains("agree-btn")) {
+                modalHeaderTitle.innerText += `\n틴친 요청을 수락하셨습니다.`;
+
+                setTimeout(() => {
+                    teenchinModalCancleBtn.innerText = `닫기`;
+                    teenchinModalCancleBtn.classList.remove("refuse-btn");
+                    teenchinModalChangedBtn.classList.remove("agree-btn");
+                }, 500);
+            }
+
             setTimeout(() => {
                 teenchinModalContainer.style.display = "none";
                 teenchinCheckModalContainer.style.animation = "popUp 0.5s";
@@ -86,6 +137,7 @@ teenchinModalBtns.forEach((teenchinModalBtn) => {
     });
 });
 
+// 2번째 확인 모달 내 확인버튼 클릭 시 발생하는 이벤트
 const checkBtn = teenchinModalWrap.querySelector(".check-btn");
 
 checkBtn.addEventListener("click", () => {
